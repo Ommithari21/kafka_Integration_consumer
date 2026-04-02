93950bf2ab16db7c35881a81789d5feec49a6d49
Om Mithari <ommithari2@gmail.com>
2026-04-02 15:52:00 +0530

    updated consumer service

--- src/main/java/com/example/consumer/Entity/Order.java
+++ src/main/java/com/example/consumer/Entity/Order.java
@@ -1,9 +1,7 @@
 package com.example.consumer;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 
 @Entity
@@ -11,6 +9,7 @@ import java.time.LocalDateTime;
 public class Order {
 
     @Id
+
     private String id;
 
     // Names match JSON keys
